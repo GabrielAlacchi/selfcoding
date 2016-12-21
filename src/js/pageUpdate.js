481b7c0bf617f5f7ef21ce0406afe313f2d2ab36
GabrielAlacchi <alacchi.g@gmail.com>
2016-12-21 17:43:03 -0500

    Added updating of css to page

---
+++ src/js/pageUpdate.js
@@ -0,0 +1,13 @@
+
+import codeStore from "./stores/CodeStore";
+
+export function attachCssListener(styleId) {
+
+  let styleTag = document.getElementById(styleId);
+  codeStore.on('new_css_line', () => {
+
+    styleTag.textContent = codeStore.getCssBody();
+
+  });
+
+}
